38cd09500ce3d84e11af8373293e423e202a0db0
Jordan Neal <jordaneal@gmail.com>
2022-04-21 09:27:35 -0700

    Switched back to useage of Dictionary and return of null in failed conversion. All methods are now static, and updated Summary.docx

--- AreaCalculatorTest/ACTest.cs
+++ AreaCalculatorTest/ACTest.cs
@@ -6,84 +6,82 @@ namespace AreaCalculatorTest
     [TestClass]
     public class ACTest
     {
-        AreaCalculator ac = new AreaCalculator();
-
         [TestMethod]
         public void SquareAreaIntSuccess()
         {
-            Assert.AreEqual(1, ac.GetSquareArea(1));
-            Assert.AreEqual(4, ac.GetSquareArea(2));
-            Assert.AreEqual(9, ac.GetSquareArea(3));
-            Assert.AreEqual(0, ac.GetSquareArea(0));
-            Assert.AreEqual(25, ac.GetSquareArea(-5));
+            Assert.AreEqual(1, AreaCalculator.GetSquareArea(1));
+            Assert.AreEqual(4, AreaCalculator.GetSquareArea(2));
+            Assert.AreEqual(9, AreaCalculator.GetSquareArea(3));
+            Assert.AreEqual(0, AreaCalculator.GetSquareArea(0));
+            Assert.AreEqual(25, AreaCalculator.GetSquareArea(-5));
         }
         [TestMethod]
         public void SquareAreaStringSuccess()
         {
-            Assert.AreEqual(1, ac.GetSquareArea("one"));
-            Assert.AreEqual(4, ac.GetSquareArea("two"));
-            Assert.AreEqual(9, ac.GetSquareArea("three"));
-            Assert.AreEqual(16, ac.GetSquareArea("four"));
-            Assert.AreEqual(25, ac.GetSquareArea("five"));
-            Assert.AreEqual(36, ac.GetSquareArea("six"));
-            Assert.AreEqual(49, ac.GetSquareArea("seven"));
-            Assert.AreEqual(64, ac.GetSquareArea("eight"));
-            Assert.AreEqual(81, ac.GetSquareArea("nine"));
-            Assert.AreEqual(100, ac.GetSquareArea("ten"));
+            Assert.AreEqual(1, AreaCalculator.GetSquareArea("one"));
+            Assert.AreEqual(4, AreaCalculator.GetSquareArea("twO"));
+            Assert.AreEqual(9, AreaCalculator.GetSquareArea("thRee"));
+            Assert.AreEqual(16, AreaCalculator.GetSquareArea("four"));
+            Assert.AreEqual(25, AreaCalculator.GetSquareArea("five"));
+            Assert.AreEqual(36, AreaCalculator.GetSquareArea("Six"));
+            Assert.AreEqual(49, AreaCalculator.GetSquareArea("Seven"));
+            Assert.AreEqual(64, AreaCalculator.GetSquareArea("eight"));
+            Assert.AreEqual(81, AreaCalculator.GetSquareArea("NINE"));
+            Assert.AreEqual(100, AreaCalculator.GetSquareArea("ten"));
         }
         [TestMethod]
         public void SquareAreaStringFailure()
         {
-            Assert.AreEqual(-1, ac.GetSquareArea("jordan"));
+            Assert.AreEqual(null, AreaCalculator.GetSquareArea("jordan"));
         }
         [TestMethod]
         public void RectangleAreaSuccess()
         {
-            Assert.AreEqual(2, ac.GetRectangleArea(1, 2));
-            Assert.AreEqual(6, ac.GetRectangleArea(2, 3));
-            Assert.AreEqual(20, ac.GetRectangleArea(4, 5));
-            Assert.AreEqual(-90, ac.GetRectangleArea(10, -9));
-            Assert.AreEqual(25, ac.GetRectangleArea(-5, -5));
+            Assert.AreEqual(2, AreaCalculator.GetRectangleArea(1, 2));
+            Assert.AreEqual(6, AreaCalculator.GetRectangleArea(2, 3));
+            Assert.AreEqual(20, AreaCalculator.GetRectangleArea(4, 5));
+            Assert.AreEqual(-90, AreaCalculator.GetRectangleArea(10, -9));
+            Assert.AreEqual(25, AreaCalculator.GetRectangleArea(-5, -5));
         }
         [TestMethod]
         public void TriangleAreaSuccess()
         {
-            Assert.AreEqual(12.5, ac.GetTriangleArea(5, 5));
-            Assert.AreEqual(25, ac.GetTriangleArea(10, 5));
-            Assert.AreEqual(16, ac.GetTriangleArea(4, 8));
-            Assert.AreEqual(13.5, ac.GetTriangleArea(3, 9));
-            Assert.AreEqual(1000, ac.GetTriangleArea(100, 20));
+            Assert.AreEqual(12.5, AreaCalculator.GetTriangleArea(5, 5));
+            Assert.AreEqual(25, AreaCalculator.GetTriangleArea(10, 5));
+            Assert.AreEqual(16, AreaCalculator.GetTriangleArea(4, 8));
+            Assert.AreEqual(13.5, AreaCalculator.GetTriangleArea(3, 9));
+            Assert.AreEqual(1000, AreaCalculator.GetTriangleArea(100, 20));
 
         }
         [TestMethod]
         public void ParallelogramAreaSuccess()
         {
-            Assert.AreEqual(25, ac.GetParallelogramArea(5, 5));
+            Assert.AreEqual(25, AreaCalculator.GetParallelogramArea(5, 5));
         }
         [TestMethod]
         public void CircleRadiusSuccess()
         {
-            Assert.AreEqual(2.5, ac.GetCircleRadius(5));
+            Assert.AreEqual(2.5, AreaCalculator.GetCircleRadius(5));
         }
         [TestMethod]
         public void CircleDiameterSuccess()
         {
-            Assert.AreEqual(10, ac.GetCircleDiameter(5));
+            Assert.AreEqual(10, AreaCalculator.GetCircleDiameter(5));
         }
         [TestMethod]
         public void SquareIntSuccess()
         {
-            Assert.AreEqual(400, ac.MultiplyInts(20));
+            Assert.AreEqual(400, AreaCalculator.MultiplyInts(20));
         }
         [TestMethod]
         public void MultiplyIntsSuccess()
         {
-            Assert.AreEqual(500, ac.MultiplyInts(5, 100));
+            Assert.AreEqual(500, AreaCalculator.MultiplyInts(5, 100));
         }
         [TestMethod]
         public void DivideByTwoAndRoundSuccess()
         {
-            Assert.AreEqual(8.5, ac.DivideByTwoAndRound(17));
+            Assert.AreEqual(8.5, AreaCalculator.DivideByTwoAndRound(17));
         }
     }
 }
--- Area_Calc_Neal/AreaCalculator.cs
+++ Area_Calc_Neal/AreaCalculator.cs
@@ -2,78 +2,64 @@
 
 namespace Area_Calc_Neal
 {
-    public class AreaCalculator
+    static public class AreaCalculator
     {
-        public double GetSquareArea(int input)
+        static public double GetSquareArea(int input)
         {
             return MultiplyInts(input);
         }
-        public double GetSquareArea(string input)
+        static public double? GetSquareArea(string input)
         {
-            string[] nums =
-            {
-                "one", "two", "three", "four", "five", "six", 
-                "seven", "eight", "nine", "ten"
-            };
+            Dictionary<string, int> stringValues =
+            new Dictionary<string, int>();
 
-            for (int i = 0; i < nums.Length; i++)
+            stringValues.Add("one", 1);
+            stringValues.Add("two", 2);
+            stringValues.Add("three", 3);
+            stringValues.Add("four", 4);
+            stringValues.Add("five", 5);
+            stringValues.Add("six", 6);
+            stringValues.Add("seven", 7);
+            stringValues.Add("eight", 8);
+            stringValues.Add("nine", 9);
+            stringValues.Add("ten", 10);
+
+            if (stringValues.TryGetValue(input.ToLower(), out int output))
             {
-                if (nums[i] == input.ToLower())
-                {
-                    return MultiplyInts(i + 1);
-                }
+                return MultiplyInts(output);
             }
 
-            // lower maintainability as the price of higher complexity
-            //Dictionary<string, int> stringValues =
-            //new Dictionary<string, int>();
-            //stringValues.Add("one",     1);
-            //stringValues.Add("two",     2);
-            //stringValues.Add("three",   3);
-            //stringValues.Add("four",    4);
-            //stringValues.Add("five",    5);
-            //stringValues.Add("six",     6);
-            //stringValues.Add("seven",   7);
-            //stringValues.Add("eight",   8);
-            //stringValues.Add("nine",    9);
-            //stringValues.Add("ten",     10);
-
-            //if (stringValues.TryGetValue(input, out int output))
-            //{
-            //    return MultiplyInts(output);
-            //}
-
-            return -1;
+            return null;
         }
-        public double GetRectangleArea(int one, int two)
+        static public double GetRectangleArea(int one, int two)
         {
             return MultiplyInts(one, two);
         }
-        public double GetTriangleArea(int one, int two)
+        static public double GetTriangleArea(int one, int two)
         {
             return DivideByTwoAndRound((double)one * two);
         }
-        public double GetParallelogramArea(int one, int two)
+        static public double GetParallelogramArea(int one, int two)
         {
             return MultiplyInts(one, two);
         }
-        public double GetCircleRadius(int input)
+        static public double GetCircleRadius(int input)
         {
             return DivideByTwoAndRound(input);
         }
-        public double GetCircleDiameter(int one)
+        static public double GetCircleDiameter(int one)
         {
             return (double)one * 2;
         }
-        public double MultiplyInts(int input)
+        static public double MultiplyInts(int input)
         {
             return (double)input * input;
         }
-        public double MultiplyInts(int one, int two)
+        static public double MultiplyInts(int one, int two)
         {
             return (double)one * two;
         }
-        public double DivideByTwoAndRound(double input)
+        static public double DivideByTwoAndRound(double input)
         {
             return Math.Round((double)input / 2, 2);
         }
